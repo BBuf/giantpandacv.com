00d5345b1838d62b67bdd8f13c2a87c47ff9869c
ftq <741032115@qq.com>
2023-03-20 13:02:08 +0800

    网站图片无法显示问题，给每个文件添加no-referrer头

---
+++ rm_refer.py
@@ -0,0 +1,32 @@
+import os
+from bs4 import BeautifulSoup
+
+# 遍历目录树，找到所有HTML文件
+def find_html_files(root_dir):
+    html_files = []
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        for filename in filenames:
+            if filename.endswith('.html') or filename.endswith('.htm'):
+                filepath = os.path.join(dirpath, filename)
+                html_files.append(filepath)
+    return html_files
+
+# 批量添加元标记
+def add_meta_tag(html_files):
+    for filepath in html_files:
+        with open(filepath, 'r', encoding='utf-8') as f:
+            html = f.read()
+        soup = BeautifulSoup(html, 'html.parser')
+        head_tag = soup.find('head')
+        if head_tag is None:
+            continue
+        meta_tag = soup.new_tag('meta')
+        meta_tag['name'] = 'referrer'
+        meta_tag['content'] = 'no-referrer'
+        head_tag.insert(0, meta_tag)
+        with open(filepath, 'w', encoding='utf-8') as f:
+            f.write(str(soup))
+
+# 执行批量添加操作
+html_files = find_html_files('site')
+add_meta_tag(html_files)
